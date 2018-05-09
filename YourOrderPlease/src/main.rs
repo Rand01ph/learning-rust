3f65c321cfb7c2ec6fa9da3f7cfbf7473c98046c
Rand01ph <tanyawei1991@gmail.com>
2018-05-09 09:10:08 +0800

    add more codewars

---
+++ YourOrderPlease/src/main.rs
@@ -0,0 +1,15 @@
+fn order(sentence: &str) -> String {
+    let mut s = String::new();
+    let mut words: Vec<&str> = sentence.split_whitespace().collect();
+    let order: Vec<i32> = sentence.matches(char::is_numeric).map(|s| s.parse().unwrap()).collect();
+    println!("{:?}", "4"-"3");
+    println!("{:?}", order);
+    return s;
+}
+
+
+#[test]
+fn returns_expected() {
+    assert_eq!(order("is2 Thi1s T4est 3a"), "Thi1s is2 3a T4est");
+    assert_eq!(order(""), "");
+}
