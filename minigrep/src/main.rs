71c7852a8f5dd7e4fd5332bd88b84dcde20d8694
Rand01ph <tanyawei1991@gmail.com>
2018-05-20 17:56:07 +0800

    update

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,23 +1,31 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("错误信息是 {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let mut f = File::open(config.filename)
-        .expect("file not found");
+    run(config);
 
+}
+
+fn run(config: Config) -> Result<(), Box<Error>>{
+
+    let mut f = File::open(config.filename)?;
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-
+    f.read_to_string(&mut contents)?;
     println!("With text:\n{}", contents);
+    Ok(())
 }
 
 struct Config {
@@ -25,10 +33,17 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("参数不足");
+        }
 
-    let query = &args[1].clone();
-    let filename = &args[2].clone();
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
-    Config {query, filename}
+        Ok(Config {query, filename})
+    }
 }
+
+
